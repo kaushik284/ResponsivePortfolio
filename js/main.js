5289cc2219a278dcc8357dfccfc64c3d734dcaf2
Kaushik <kaushik.mitra28@gmail.com>
2020-04-15 16:49:22 +0530

    Updates

---
+++ js/main.js
@@ -0,0 +1,44 @@
+// Select items from DOM
+const menuBtn = document.querySelector('.menu-btn');
+
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const menuBranding = document.querySelector('.menu-branding');
+
+//all elements 
+const navItems = document.querySelectorAll('.nav-item');
+
+// Set the initial state of the menu
+let showMenu = false;
+
+//event listener
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu() {
+    if (!showMenu) {
+        //add close class to button
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+
+        //loop for nav-item
+        navItems.forEach(item => item.classList.add('show'));
+
+        //Set menu state 
+        showMenu = true;
+    }
+    else {
+        //remove classes added in if
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+
+        //loop for nav-item
+        navItems.forEach(item => item.classList.remove('show'));
+
+        //Set menu state 
+        showMenu = false;
+    }
+}
